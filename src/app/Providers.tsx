905c81c1f5a139b6378d5768452091c5c577ace9
Satya Sandeep <satyasandeep786@gmail.com>
2024-09-10 01:56:35 +0530

    fix: wagmi issue

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,31 +1,7 @@
 'use client';
 
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, polygonMumbai, sepolia } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
 import { MetaMaskProvider } from '@/hooks';
 
-const { chains, provider } = configureChains([mainnet, polygon, sepolia, polygonMumbai], [publicProvider()]);
-
-const { connectors } = getDefaultWallets({
-  appName: 'NextJS Snap',
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-});
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <MetaMaskProvider>{children}</MetaMaskProvider>
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
+  return <MetaMaskProvider>{children}</MetaMaskProvider>;
 }
