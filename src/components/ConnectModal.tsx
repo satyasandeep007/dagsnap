2415535c1f5d205890be477399c13610b0432cc8
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2024-09-10 23:58:32 +0530

    fix: Modal UI

--- src/components/ConnectModal.tsx
+++ src/components/ConnectModal.tsx
@@ -41,7 +41,7 @@ const ConnectModal: React.FC<ConnectModalProps> = ({ onClose }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white rounded-lg p-6 w-72 h-96 shadow-xl">
+      <div className="bg-white rounded-lg p-6 w-[22rem] h-[30rem] shadow-xl">
         <div className="flex justify-end">
           <button onClick={onClose} className="text-gray-400 hover:text-gray-600">
             <svg
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,8 @@ const Header = ({ userAddress }: { userAddress: any }) => {
       <div className="flex items-center space-x-2 ">
         <div className="w-40 h-10 flex items-center justify-center">
           <Image src={logo} alt="Company Logo" width={30} height={30} className="object-contain max-w-full" />
-          <span className="text-2xl px-2 font-thin">DAGSNAP</span>
+          <span className="text-2xl pl-2 font-bold">DAG</span>
+          <span className="text-2xl pr-2 font-thin">SNAP</span>
         </div>
       </div>
     </header>
