0743b2b8730e7995d4a4c23ca17e06ca80338353
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2024-09-11 13:01:23 +0530

    fix: Update Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '@/images/dag.png';
+import logo from '@/images/dglogo.png';
 import Image from 'next/image';
 
 const Header = ({ userAddress }: { userAddress: any }) => {
@@ -7,9 +7,9 @@ const Header = ({ userAddress }: { userAddress: any }) => {
     <header className="flex justify-between items-center mb-6  border-indigo-200/50 border-b pb-4">
       <div className="flex items-center space-x-2 ">
         <div className="w-40 h-10 flex items-center justify-center">
-          <Image src={logo} alt="Company Logo" width={30} height={30} className="object-contain max-w-full" />
-          <span className="text-2xl pl-2 font-bold">DAG</span>
-          <span className="text-2xl pr-2 font-thin">SNAP</span>
+          <Image src={logo} alt="Company Logo" />
+          {/* <span className="text-2xl pl-2 font-bold">DAG</span>
+          <span className="text-2xl pr-2 font-thin">SNAP</span> */}
         </div>
       </div>
     </header>
